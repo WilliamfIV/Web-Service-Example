7b733034fa0ae631256056a86d78b1f01eeab0ff
William Henry Fulton IV <will.fulton95@gmail.com>
2017-07-04 00:24:16 -0400

    AFNetwork added to 'web server example' project

---
+++ bridge.h
@@ -0,0 +1,13 @@
+//
+//  bridge.h
+//  Web Service Example
+//
+//  Created by William H Fulton IV on 7/4/17.
+//  Copyright © 2017 IVdev. All rights reserved.
+//
+#import "AFNetworking/AFNetworking.h"
+#ifndef bridge_h
+#define bridge_h
+
+
+#endif /* bridge_h */
