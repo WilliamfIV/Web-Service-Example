c00c9e89ac50a1ccaf1c478a4e69118d2d6a3a55
William Henry Fulton IV <will.fulton95@gmail.com>
2017-07-03 23:25:14 -0400

    Initial Commit

---
+++ Web Service Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Web Service Example
+//
+//  Created by William H Fulton IV on 7/3/17.
+//  Copyright © 2017 IVdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
