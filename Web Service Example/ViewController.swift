a7e01f91c554b07fbc15210a36d66d32dba6fcf8
William Henry Fulton IV <will.fulton95@gmail.com>
2017-07-07 14:57:00 -0400

    stuff from assignment 3 added

--- Web Service Example/ViewController.swift
+++ Web Service Example/ViewController.swift
@@ -15,14 +15,29 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  
+        
+        //Intstantiated 'AFTTPSessionManager' to make http requests in this code
+        //If the get request is successful then the 'success' closure with be executed
+    
+    let manager = AFHTTPSessionManager()
+    
+    manager.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=json&units=metric&cnt=1&appid=1cfc3768d72b5eba2ed50a80f10ea48a",
+        parameters: nil,
+        progress: nil,
+        success: {  (operation: URLSessionDataTask, responseObject: Any?) in
+            if let responseObject = responseObject {
+                print("Response: " + (responseObject as AnyObject).description)
+            }
+    })  { (operation: URLSessionDataTask?, error: Error) in
+        print("Error: " + error.localizedDescription)
     }
+}
 
+    
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
